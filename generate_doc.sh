c461cc4f245135a01e908253cf5f1db18e42c61f
Ash_Crow <sylvain@ashtree.eu>
2021-09-28 10:31:27 +0200

    Script to generate the documentation

---
+++ generate_doc.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+poetry run python manage.py collectstatic --noinput
+poetry run python manage.py distill-local docs --force
+touch docs/.nojekyll
+mv docs/django-dsfr/* docs/
+rmdir docs/django-dsfr
