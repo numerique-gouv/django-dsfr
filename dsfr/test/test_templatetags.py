1fab19e3e61b4dd1ebc455a1d85648730a47d534
Sylvain Boissel <sylvain@ashtree.eu>
2022-09-14 18:20:11 +0200

    Minor accessibility improvements (#19)
    * Attaching modal close button to modal title
    
    * Fix minor accessibility issues
    
    * Fix accessibility detail

--- dsfr/test/test_templatetags.py
+++ dsfr/test/test_templatetags.py
@@ -168,7 +168,6 @@ class DsfrBadgeTagTest(SimpleTestCase):
 
     def test_badge_tag_rendered(self):
         rendered_template = self.template_to_render.render(self.context)
-        print(rendered_template)
         self.assertInHTML(
             """
             <p class="fr-badge fr-badge--success">badge label</p>
@@ -452,7 +451,7 @@ class DsfrLinkTagTest(SimpleTestCase):
             href="http://example.com"
             target="_blank" rel="noopener noreferrer"
             >
-            Label of the link item
+            Label of the link item <span class="sr-only">Ouvre une nouvelle fenêtre</span>
             </a>
             """,
             rendered_template,
@@ -492,7 +491,7 @@ class DsfrQuoteTagTest(SimpleTestCase):
                         <cite>Système de Design de l&#x27;État</cite>
                     </li>
                     <li>Detail sans lien</li>
-                    <li><a target="_blank" rel="noopener noreferrer" href="https://template.incubateur.net/">Detail avec lien</a></li>
+                    <li><a target="_blank" rel="noopener noreferrer" href="https://template.incubateur.net/">Detail avec lien <span class="sr-only">Ouvre une nouvelle fenêtre</span></a></li>
                     </ul>
                     <div class="fr-quote__image">
                     <img src="https://via.placeholder.com/150x150" class="fr-responsive-img" alt="" />
