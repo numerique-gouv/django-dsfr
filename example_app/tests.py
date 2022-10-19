a5f6d04caa98e188d600cee0ae7f4a23bc424b6e
Sylvain Boissel <sylvain@boissel.dev>
2022-10-19 09:59:44 +0200

    Add test for the new feature

--- example_app/tests.py
+++ example_app/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+from http import HTTPStatus
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+
+
+class DsfrBaseForm(TestCase):
+
+    sample_data = {"user_name": "Example Name", "sample_number": 5, "sample_json": "{}"}
+
+    def test_valid_form(self):
+        response = self.client.post(
+            reverse("page_form"),
+            data=self.sample_data,
+        )
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_form_sets_autofocus_on_first_error(self):
+        sample_data = self.sample_data
+        sample_data["sample_number"] = -5
+        response = self.client.post(
+            reverse("page_form"),
+            data=sample_data,
+        )
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertContains(
+            response,
+            """<input type="number" name="sample_number" value="-5"
+            class="fr-input" autofocus="" required id="id_sample_number">""",
+            html=True,
+        )
+        self.assertContains(response, "Merci d’entrer un nombre positif", html=True)
