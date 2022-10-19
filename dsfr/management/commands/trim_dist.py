0fd04bf475df23ca5d33b5f335e76fe52561b067
Sylvain Boissel <sylvain@ashtree.eu>
2022-10-19 15:52:25 +0200

    Remove mention of the deleted map files (#29)

--- dsfr/management/commands/trim_dist.py
+++ dsfr/management/commands/trim_dist.py
@@ -11,32 +11,47 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         print("Delete CSS map files and non-minified CSS files")
-        count = 0
+        deleted_files = 0
         all_css_map_files = self.get_files_by_ext("*.css.map")
         for file in all_css_map_files:
             os.remove(file)
-            count += 1
+            deleted_files += 1
 
         all_full_css_files = self.get_non_minified_files_by_ext("*.css", ".min.css")
         for file in all_full_css_files:
             os.remove(file)
-            count += 1
+            deleted_files += 1
 
-        print(f"{count} files deleted.")
+        print(f"{deleted_files} files deleted.")
 
         print("Delete JS map files and non-minified files")
-        count = 0
-        all_css_map_files = self.get_files_by_ext("*.js.map")
-        for file in all_css_map_files:
+        deleted_files = 0
+        all_js_map_files = self.get_files_by_ext("*.js.map")
+        for file in all_js_map_files:
             os.remove(file)
-            count += 1
+            deleted_files += 1
 
-        all_full_css_files = self.get_non_minified_files_by_ext("*.js", ".min.js")
-        for file in all_full_css_files:
+        all_full_js_files = self.get_non_minified_files_by_ext("*.js", ".min.js")
+        for file in all_full_js_files:
             os.remove(file)
-            count += 1
+            deleted_files += 1
 
-        print(f"{count} files deleted.")
+        print(f"{deleted_files} files deleted.")
+
+        print("Removing mentions of deleted files")
+        edited_files = 0
+        all_minified_js_files = self.get_files_by_ext("*.min.js")
+        for filename in all_minified_js_files:
+            with open(filename, "r+") as file:
+                original_content = file.readlines()
+                file.seek(0)
+                for line in original_content:
+                    if "//# sourceMappingURL=" not in line:
+                        file.write(line)
+                file.truncate()
+                edited_files += 1
+
+        print(f"{edited_files} files edited.")
 
     def get_files_by_ext(self, extension) -> list:
         return [
