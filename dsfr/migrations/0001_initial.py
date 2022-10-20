0f9409eac2a18d3dbdadc63d52da4fa3a57ce10f
Sylvain Boissel <sylvain@boissel.dev>
2022-10-20 09:35:17 +0200

    black

--- dsfr/migrations/0001_initial.py
+++ dsfr/migrations/0001_initial.py
@@ -7,26 +7,93 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='DsfrConfig',
+            name="DsfrConfig",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('header_brand', models.CharField(default='République française', max_length=200, verbose_name='Institution (en-tête)')),
-                ('header_brand_html', models.CharField(default='République<br />française', max_length=200, verbose_name='Institution avec césure (en-tête)')),
-                ('footer_brand', models.CharField(default='République française', max_length=200, verbose_name='Institution (pied)')),
-                ('footer_brand_html', models.CharField(default='République<br />française', max_length=200, verbose_name='Institution avec césure (pied)')),
-                ('site_title', models.CharField(default='Titre du site', max_length=200, verbose_name='Titre du site')),
-                ('site_tagline', models.CharField(default='Sous-titre du site', max_length=200, verbose_name='Sous-titre du site')),
-                ('footer_description', models.TextField(default='', verbose_name='Description')),
-                ('mourning', models.BooleanField(default=False, verbose_name='Mise en berne')),
-                ('accessibility_status', models.CharField(choices=[('FULL', 'complètement'), ('PART', 'partiellement'), ('NOT', 'non')], default='non', max_length=4, verbose_name='Statut de conformité de l’accessibilité')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                (
+                    "header_brand",
+                    models.CharField(
+                        default="République française",
+                        max_length=200,
+                        verbose_name="Institution (en-tête)",
+                    ),
+                ),
+                (
+                    "header_brand_html",
+                    models.CharField(
+                        default="République<br />française",
+                        max_length=200,
+                        verbose_name="Institution avec césure (en-tête)",
+                    ),
+                ),
+                (
+                    "footer_brand",
+                    models.CharField(
+                        default="République française",
+                        max_length=200,
+                        verbose_name="Institution (pied)",
+                    ),
+                ),
+                (
+                    "footer_brand_html",
+                    models.CharField(
+                        default="République<br />française",
+                        max_length=200,
+                        verbose_name="Institution avec césure (pied)",
+                    ),
+                ),
+                (
+                    "site_title",
+                    models.CharField(
+                        default="Titre du site",
+                        max_length=200,
+                        verbose_name="Titre du site",
+                    ),
+                ),
+                (
+                    "site_tagline",
+                    models.CharField(
+                        default="Sous-titre du site",
+                        max_length=200,
+                        verbose_name="Sous-titre du site",
+                    ),
+                ),
+                (
+                    "footer_description",
+                    models.TextField(default="", verbose_name="Description"),
+                ),
+                (
+                    "mourning",
+                    models.BooleanField(default=False, verbose_name="Mise en berne"),
+                ),
+                (
+                    "accessibility_status",
+                    models.CharField(
+                        choices=[
+                            ("FULL", "complètement"),
+                            ("PART", "partiellement"),
+                            ("NOT", "non"),
+                        ],
+                        default="non",
+                        max_length=4,
+                        verbose_name="Statut de conformité de l’accessibilité",
+                    ),
+                ),
             ],
             options={
-                'verbose_name': 'Configuration',
+                "verbose_name": "Configuration",
             },
         ),
     ]
--- dsfr/migrations/0002_auto_20211209_1557.py
+++ dsfr/migrations/0002_auto_20211209_1557.py
@@ -6,48 +6,87 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dsfr', '0001_initial'),
+        ("dsfr", "0001_initial"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='dsfrconfig',
-            name='accessibility_status',
-            field=models.CharField(choices=[('FULL', 'complètement'), ('PART', 'partiellement'), ('NOT', 'non')], default='NOT', max_length=4, verbose_name='Statut de conformité de l’accessibilité'),
+            model_name="dsfrconfig",
+            name="accessibility_status",
+            field=models.CharField(
+                choices=[
+                    ("FULL", "complètement"),
+                    ("PART", "partiellement"),
+                    ("NOT", "non"),
+                ],
+                default="NOT",
+                max_length=4,
+                verbose_name="Statut de conformité de l’accessibilité",
+            ),
         ),
         migrations.AlterField(
-            model_name='dsfrconfig',
-            name='footer_brand',
-            field=models.CharField(blank=True, default='République française', max_length=200, verbose_name='Institution (pied)'),
+            model_name="dsfrconfig",
+            name="footer_brand",
+            field=models.CharField(
+                blank=True,
+                default="République française",
+                max_length=200,
+                verbose_name="Institution (pied)",
+            ),
         ),
         migrations.AlterField(
-            model_name='dsfrconfig',
-            name='footer_brand_html',
-            field=models.CharField(blank=True, default='République<br />française', max_length=200, verbose_name='Institution avec césure (pied)'),
+            model_name="dsfrconfig",
+            name="footer_brand_html",
+            field=models.CharField(
+                blank=True,
+                default="République<br />française",
+                max_length=200,
+                verbose_name="Institution avec césure (pied)",
+            ),
         ),
         migrations.AlterField(
-            model_name='dsfrconfig',
-            name='footer_description',
-            field=models.TextField(blank=True, default='', verbose_name='Description'),
+            model_name="dsfrconfig",
+            name="footer_description",
+            field=models.TextField(blank=True, default="", verbose_name="Description"),
         ),
         migrations.AlterField(
-            model_name='dsfrconfig',
-            name='header_brand',
-            field=models.CharField(blank=True, default='République française', max_length=200, verbose_name='Institution (en-tête)'),
+            model_name="dsfrconfig",
+            name="header_brand",
+            field=models.CharField(
+                blank=True,
+                default="République française",
+                max_length=200,
+                verbose_name="Institution (en-tête)",
+            ),
         ),
         migrations.AlterField(
-            model_name='dsfrconfig',
-            name='header_brand_html',
-            field=models.CharField(blank=True, default='République<br />française', max_length=200, verbose_name='Institution avec césure (en-tête)'),
+            model_name="dsfrconfig",
+            name="header_brand_html",
+            field=models.CharField(
+                blank=True,
+                default="République<br />française",
+                max_length=200,
+                verbose_name="Institution avec césure (en-tête)",
+            ),
         ),
         migrations.AlterField(
-            model_name='dsfrconfig',
-            name='site_tagline',
-            field=models.CharField(blank=True, default='Sous-titre du site', max_length=200, verbose_name='Sous-titre du site'),
+            model_name="dsfrconfig",
+            name="site_tagline",
+            field=models.CharField(
+                blank=True,
+                default="Sous-titre du site",
+                max_length=200,
+                verbose_name="Sous-titre du site",
+            ),
         ),
         migrations.AlterField(
-            model_name='dsfrconfig',
-            name='site_title',
-            field=models.CharField(blank=True, default='Titre du site', max_length=200, verbose_name='Titre du site'),
+            model_name="dsfrconfig",
+            name="site_title",
+            field=models.CharField(
+                blank=True,
+                default="Titre du site",
+                max_length=200,
+                verbose_name="Titre du site",
+            ),
         ),
     ]
