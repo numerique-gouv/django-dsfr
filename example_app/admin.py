f216278d50679bf70cc79867ce74094b8e93e247
Sylvain Boissel <sylvain@boissel.dev>
2023-04-15 22:06:29 +0200

    black on changed files

--- example_app/admin.py
+++ example_app/admin.py
@@ -8,29 +8,31 @@ class AuthorAdmin(admin.ModelAdmin):
     """
     Admin model for Author
     """
-    
-    list_display=(
+
+    list_display = (
         "first_name",
         "last_name",
         "birth_date",
     )
-    
+
+
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
     """
     Admin model for Genre
     """
-    
-    list_display=(
+
+    list_display = (
         "code",
         "designation",
         "help_text",
     )
+
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     """
     Admin model for Book
     """
-    
+
     pass
--- example_app/views.py
+++ example_app/views.py
@@ -220,16 +220,16 @@ class AuthorCreateView(CreateView):
     model = Author
     form_class = AuthorCreateForm
     formset = BookCreateFormSet  # /!\ Your formset factory
-    template_name = 'example_app/example-form.html'  # /!\ Your template needs to extends form-base.html. If you use formset, you template needs to include another template which extends formset-base.html
-    
+    template_name = "example_app/example-form.html"  # /!\ Your template needs to extends form-base.html. If you use formset, you template needs to include another template which extends formset-base.html
+
     def get(self, request, *args, **kwargs):
         instance = None
-        try :
+        try:
             if self.object:
                 instance = self.object
         except Exception:
-            self.object=None
-            
+            self.object = None
+
         form_class = self.get_form_class()
         form = self.get_form(form_class)
         formset = BookCreateFormSet(instance=instance)
@@ -238,64 +238,69 @@ class AuthorCreateView(CreateView):
         return self.render_to_response(
             self.get_context_data(form=form, formset=formset)
         )
-    
+
     def get_context_data(self, **kwargs):
         context = super(AuthorCreateView, self).get_context_data(**kwargs)
         book_formhelper = BookCreateFormHelper()
-        
+
         instance = None
         try:
             if self.object:
                 instance = self.object
         except Exception:
-            self.object=None
-    
+            self.object = None
+
         # /!\ Pass your form, formset and helper to the context
         if self.request.POST:
-            context['form'] = self.form_class(self.request.POST)
-            context['formset'] = BookCreateFormSet(self.request.POST, self.request.FILES, instance=instance)
-            context['book_formhelper'] = book_formhelper
+            context["form"] = self.form_class(self.request.POST)
+            context["formset"] = BookCreateFormSet(
+                self.request.POST, self.request.FILES, instance=instance
+            )
+            context["book_formhelper"] = book_formhelper
         else:
-            context['form'] = self.form_class()
-            context['formset'] = BookCreateFormSet(instance=instance)
-            context['book_formhelper'] = book_formhelper
-        
+            context["form"] = self.form_class()
+            context["formset"] = BookCreateFormSet(instance=instance)
+            context["book_formhelper"] = book_formhelper
+
         # /!\ Don't forget your dsfr button
-        context['btn_submit'] = {
-                                    "label": "Soumettre",
-                                    "onclick": "",
-                                    "type":"submit",
-                                }
+        context["btn_submit"] = {
+            "label": "Soumettre",
+            "onclick": "",
+            "type": "submit",
+        }
         return context
-    
-    
+
     def post(self, request, *args, **kwargs):
         instance = None
         try:
             if self.object:
                 instance = self.object
         except Exception:
-            self.object=None
+            self.object = None
 
         form_class = self.get_form_class()
         form = self.get_form(form_class)
-        formset = BookCreateFormSet(self.request.POST, self.request.FILES, instance=instance)
-        
-        if (form.is_valid() and formset.is_valid()):
+        formset = BookCreateFormSet(
+            self.request.POST, self.request.FILES, instance=instance
+        )
+
+        if form.is_valid() and formset.is_valid():
             return self.form_valid(form, formset)
         return self.form_invalid(form, formset)
-    
+
     def form_valid(self, form, formset):
         """
         Called if all forms are valid. Creates a Author instance along
         with associated books and then redirects to a success page.
         """
-        
+
         self.object = form.save()
-        formset.instance = self.object  # /!\ Before saving formset, link it to the object created with the main form
+        formset.instance = (
+            self.object
+        )  # /!\ Before saving formset, link it to the object created with the main form
         formset.save()
 
-        return HttpResponse('Success !')
+        return HttpResponse("Success !")
 
     def form_invalid(self, form, formset):
         """
