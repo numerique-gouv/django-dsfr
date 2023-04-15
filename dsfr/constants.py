e06eacc961a40de43107001a930e17710eb72184
Sylvain Boissel <sylvain@boissel.dev>
2023-04-15 22:04:34 +0200

    Revert changes to constants.py (not sure what is going on here)

--- dsfr/constants.py
+++ dsfr/constants.py
@@ -5,12 +5,7 @@
 #### JS
 # dsfr/dist/dsfr/dsfr.module.min.js
 INTEGRITY_JS_MODULE = (
-<<<<<<< HEAD
     "sha384-I2h3dQPRm8wpUZxuuDoCO5k+q6jvFHVAcTkMeJJq/noEkqKDlj6TfbB/TRUgp0zO"
-=======
-    #"sha384-hsT8G3ewRLMSn+hcsZeqdjIsqjqMh/pD8TTc8egEcN4MpeNQDZ+7EqVXDW0zVfYm"
-    "sha384-dz/j9V96lYjBoF1z9LcwX9L4n5fy27EcB+iRDDHWA+ZIKbaA2nrB23TSD4An/LkQ"
->>>>>>> a616374 (work on form and formset without jquery)
 )
 # dsfr/dist/dsfr/dsfr.nomodule.min.js
 INTEGRITY_JS_NOMODULE = (
@@ -20,21 +15,11 @@ INTEGRITY_JS_NOMODULE = (
 #### CSS
 # dsfr/dist/dsfr/dsfr.min.css
 INTEGRITY_CSS = (
-<<<<<<< HEAD
     "sha384-LtcfS1K2I3jMUhzjxpeUO3UUiin8SpxX5rODWf05CrGtS44FrX8oAYCPVaFbM10M"
 )
 # dsfr/dist/utility/icons/icons.min.css
 INTEGRITY_CSS_ICONS = (
     "sha384-STqXIfO4sjaf/YUpwUezCvsCRtEmzt2vrpk5cixx+BPM6SWpOC6K8Bu5b9Svd3sV"
-=======
-    #"sha384-7z0V109kCjLc5JiwfCaR+gM7rwAw8pWsrgISPTlaD3B77TwopmDN3DQ20n36NMB2"
-    "sha384-L6i86cqRQiI1sLFSQmfcQxuKzrHiBS4BynnVXximeBgzKhXRu5zUWFbG98b3YXiz"
-)
-# dsfr/dist/utility/icons/icons.min.css
-INTEGRITY_CSS_ICONS = (
-    #"sha384-J7lCkOU9dOy1LLABqcy542lznn5TyxJnWuFXMse5v7POyQlh95Esaw7RtKwNAHIP"
-    "sha384-ImL+nPhu0wEa1fkqlFllQ5fpT5q59IUKBNgXmMHkYRi6zja2VjyGVrwt4PGfQPwd"
->>>>>>> a616374 (work on form and formset without jquery)
 )
 
 #### Favicon
