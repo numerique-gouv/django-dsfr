db4f6f4226f599fe72b2c9d2839d452ff1d0b6c9
Ash-Crow <Ash-Crow@users.noreply.github.com>
2024-04-02 13:38:31 +0000

    Deploying to gh-pages from  @ 8cf895a30863f1e73dbdd5966f92f3bd5291a42a 🚀

--- static/js/common.js
+++ static/js/common.js
@@ -1,6 +1,6 @@
 const language_selectors = document.querySelectorAll(".fr-translate__language")
 
 language_selectors.forEach(el => el.addEventListener("click", event => {
-    document.cookie = "django_language=" + el.lang;
+    document.cookie = "django_language=" + el.lang + ";Path=\"/django-dsfr\";SameSite=Strict"
     window.location.reload()
 }));
