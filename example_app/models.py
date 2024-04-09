2b362d3cdbe2c629719dda85e234175000ade7ae
Sylvain Boissel <sylvain@boissel.dev>
2024-04-09 11:27:07 +0200

    Add translation of configuration

--- example_app/models.py
+++ example_app/models.py
@@ -1,29 +1,40 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
 
 # These are example models to show how form and formset can work
 class Author(models.Model):
-    first_name = models.CharField("Prénom", max_length=250, null=False, blank=False)
-    last_name = models.CharField("Nom", max_length=250, null=False, blank=False)
-    birth_date = models.DateField()
+    first_name = models.CharField(
+        _("First name"), max_length=250, null=False, blank=False
+    )
+    last_name = models.CharField(
+        _("Last name"), max_length=250, null=False, blank=False
+    )
+    birth_date = models.DateField(_("Birth date"), null=True, blank=True)
+
+    class Meta:
+        verbose_name = _("Author")
 
 
 class Genre(models.Model):
-    code = models.CharField("Code", max_length=15, null=False, blank=False)
+    code = models.CharField(_("Code"), max_length=15, null=False, blank=False)
     designation = models.CharField(
-        "Désignation", max_length=250, null=False, blank=False
+        _("Designation"), max_length=250, null=False, blank=False
     )
-    help_text = models.CharField("Texte d’aide", max_length=250, null=True, blank=True)
+    help_text = models.CharField(_("Help text"), max_length=250, blank=True)
 
     def __str__(self):
-        return self.designation
+        return str(self.designation)
+
+    class Meta:
+        verbose_name = _("Genre")
 
 
 BOOK_FORMAT = (
-    ("PAPER", "Papier"),
-    ("NUM", "Numérique"),
+    ("PAPER", _("Paper")),
+    ("NUM", _("Digital")),
 )
 
 
@@ -31,11 +42,12 @@ class Book(models.Model):
     author = models.ForeignKey(
         Author, on_delete=models.CASCADE, null=False, blank=False
     )
-    title = models.CharField("Titre", max_length=250, null=False, blank=False)
-    number_of_pages = models.CharField(
-        "Nombre de pages", max_length=6, null=True, blank=True
-    )
+    title = models.CharField(_("Title"), max_length=250, null=False, blank=False)
+    number_of_pages = models.CharField(_("Number of pages"), max_length=6, blank=True)
     book_format = models.CharField(
-        "Format", choices=BOOK_FORMAT, max_length=10, null=True, blank=True
+        _("Format"), choices=BOOK_FORMAT, max_length=10, blank=True
     )
     genre = models.ManyToManyField(Genre)
+
+    class Meta:
+        verbose_name = _("Book")
