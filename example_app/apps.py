a5c467828b6e3c14c5efe2cec25357c7ba8d7b28
Sylvain Boissel <sylvain@boissel.dev>
2024-04-09 15:56:14 +0200

    Translate app name

--- example_app/apps.py
+++ example_app/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
+from django.utils.translation import gettext_lazy as _
+
 
 class ExampleAppConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
+    default_auto_field = "django.db.models.BigAutoField"  # type: ignore
     name = "example_app"
+    verbose_name = _("Example app")
