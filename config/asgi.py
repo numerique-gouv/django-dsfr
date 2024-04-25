f097e004c92fa4f40e089529fea9debba143a101
Sylvain Boissel <sylvain@boissel.dev>
2024-04-25 14:28:47 +0200

    Renamed django project to config

--- config/asgi.py
+++ config/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for example project.
+ASGI config for django-dsfr project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "example.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_asgi_application()
