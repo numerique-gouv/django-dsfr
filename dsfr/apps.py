5ab9505084d2c271823bdf938c1bb7fc9676f1e6
Sylvain Boissel <sylvain@boissel.dev>
2025-04-30 18:11:57 +0200

    Minor fixes

--- dsfr/apps.py
+++ dsfr/apps.py
@@ -2,6 +2,6 @@ from django.apps import AppConfig
 
 
 class DsfrConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
+    default_auto_field = "django.db.models.BigAutoField"  # type: ignore
     name = "dsfr"
     verbose_name = "Système de design de l’État"
